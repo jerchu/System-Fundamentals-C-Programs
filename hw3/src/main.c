0e8dcdd274e6a1233095d960b650df7a4a7ef413
Jeremy Chu <jerchu@cs.stonybrook.edu>
2017-10-11 04:35:00 -0400

    initializied seg_list in malloc and re added sfutil.o

--- hw3/src/main.c
+++ hw3/src/main.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
 #include "sfmm.h"
+#include "debug.h"
 
 int main(int argc, char const *argv[]) {
 
     sf_mem_init();
 
+
+
     double* ptr = sf_malloc(sizeof(double));
+
+    //sf_snapshot();
 
     *ptr = 320320320e-320;
 
-    printf("%f\n", *ptr);
+    //printf("%f\n", *ptr);
 
-    sf_free(ptr);
+    //sf_free(ptr);
 
     sf_mem_fini();
 
--- hw3/src/sfmm.c
+++ hw3/src/sfmm.c
@@ -6,6 +6,7 @@
 #include "sfmm.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "debug.h"
 
 /**
  * You should store the heads of your free lists in these variables.
@@ -22,6 +23,44 @@ free_list seg_free_list[4] = {
 int sf_errno = 0;
 
 void *sf_malloc(size_t size) {
+    if(size == 0 || size > PAGE_SZ *4)
+    {
+        //sf_errno = EINVAL
+        return NULL;
+    }
+    if(get_heap_end() - get_heap_start() < PAGE_SZ)
+    {
+        sf_sbrk();
+        get_heap_start();
+        for(int i = 3; i >= 0; i--)
+        {
+            if(seg_free_list[i].min < PAGE_SZ)
+            {
+
+                sf_free_header *free_header = get_heap_start();
+                //sf_header header = free_header->header;
+                free_header->header.allocated = 0;
+                free_header->header.padded = 0;
+                free_header->header.two_zeroes = 0;
+                free_header->header.block_size = 4096 / 16;
+                debug("%d", free_header->header.block_size);
+                free_header->next = NULL;
+                free_header->prev = NULL;
+                sf_footer *footer = (get_heap_end() - 8);
+                footer->allocated = 0;
+                footer->padded = 0;
+                footer->two_zeroes = 0;
+                footer->block_size = 4096 / 16;
+                footer->requested_size = 4096;
+                //sf_header *header = free_header->header
+                seg_free_list[i].head = free_header;
+                debug("%d", i);
+                sf_blockprint(seg_free_list[i].head);
+                break;
+            }
+        }
+    }
+    sf_snapshot();
 	return NULL;
 }
 
