de17cb798ca2d79dc26c988676c4f19df7109d64
Jeremy Chu <jerchu@cs.stonybrook.edu>
2017-11-02 04:31:27 -0400

    added redirection functionality

--- hw4/include/sfish.h
+++ hw4/include/sfish.h
@@ -3,7 +3,17 @@
 
 /* Format Strings */
 #define EXEC_NOT_FOUND "sfish: %s: command not found\n"
+#define BUILTIN_ERROR  "sfish builtin error: %s\n"
+#define SYNTAX_ERROR   "sfish syntax error: %s\n"
+#define EXEC_ERROR     "sfish exec error: %s\n"
 #define JOBS_LIST_ITEM "[%d] %s\n"
 #define STRFTIME_RPRMT "%a %b %e, %I:%M%p"
+
+/* redirection constants */
+#define NO_REDIRECTION 0
+#define PIPING_REDIRECTION 1
+#define INPUT_REDIRECTION 2
+#define OUTPUT_REDIRECTION 3
+
 
 #endif
--- hw4/src/main.c
+++ hw4/src/main.c
@@ -2,6 +2,7 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -13,32 +14,50 @@
 #include "sfish.h"
 #include "debug.h"
 
-typedef struct ArgStruct ArgStruct;
+/*typedef struct ExecStruct ExecStruct;
 
-struct ArgStruct{
-    char *currarg;
-    ArgStruct *nextarg;
-};
+struct ExecStruct{
+    char *execpath;
+    char **vargs;
+    char *redirectout = NULL;
+    int outpos = -1;
+    char *redirectin = NULL;
+    int inpos = -1;
+};*/
 
 sigset_t mask, prev;
 char currentDir[256];
 char prevDir[256] = {0};
+pid_t childpid;
+pid_t pid;
+char *tokptr;
+int pipefd1[2] = {-1, -1};
+int pipefd2[2] = {-1 ,-1};
+char *outpos;
+char *inpos;
 
 int execute(const char *pathname, char *vargs[]);
-int prepexecute(char *currarg, char *inputptr);
+int prepexecute(char *execpath, char *currarg, char *inputptr);
+int findexecutable(char *input);
+int checkredirection(char *currarg);
+char *trimwhitespace(char *token);
+void exitfork(int success, int inputfd, int outputfd);
 
 void sigchld_handler(int s)
 {
     int olderrno = errno;
-    pid_t pid = wait(NULL);
-    pid = pid;
+    pid = waitpid(childpid, NULL, 0);
     errno = olderrno;
+}
+void sigint_handler(int s)
+{
 }
 
 int main(int argc, char *argv[], char* envp[]) {
     signal(SIGCHLD, sigchld_handler);
     sigemptyset(&mask);
     sigaddset(&mask, SIGCHLD);
+    //sigaddset(&mask, SIGINT);
     char* input;
     bool exited = false;
 
@@ -61,10 +80,10 @@ int main(int argc, char *argv[], char* envp[]) {
         //printf("%s\n", currentDir);
         if(homeDir){
             sprintf(prompt, "~");
-            strcat(strcat(prompt, homeDir + strlen(home)), " :: jerchu >>");
+            strcat(strcat(prompt, homeDir + strlen(home)), " :: jerchu >> ");
         }
         else
-            strcat(strcat(prompt, currentDir), " :: jerchu >>");
+            strcat(strcat(prompt, currentDir), " :: jerchu >> ");
         input = readline(prompt);
 
         write(1, "\e[s", strlen("\e[s"));
@@ -74,6 +93,10 @@ int main(int argc, char *argv[], char* envp[]) {
 
         // If EOF is read (aka ^D) readline returns NULL
         if(input == NULL) {
+            printf("\n");
+            continue;
+        }
+        else if(strlen(input) == 0){
             continue;
         }
 
@@ -114,7 +137,7 @@ int main(int argc, char *argv[], char* envp[]) {
                     printf("%s\n", currentDir);
                 }
                 else{
-                    printf("No previous directory\n");
+                    printf(BUILTIN_ERROR, "cd: No previous directory");
                 }
             }
 
@@ -149,7 +172,9 @@ int main(int argc, char *argv[], char* envp[]) {
                         getcwd(currentDir, 256);
                     }
                     else{
-                        printf("there was a problem going to directory %s\n", newdir);
+                        char errstr[1000] = {0};
+                        sprintf(errstr, "cd: there was a problem going to directory %s\n", newdir);
+                        printf(BUILTIN_ERROR, errstr);
                     }
                 }
                 //printf(EXEC_NOT_FOUND, input);
@@ -159,51 +184,11 @@ int main(int argc, char *argv[], char* envp[]) {
             printf("%s\n", currentDir);
         }
         else if(!exited){
-            //ArgStruct *args;
-            char *inputptr;
-            char *currarg = strtok_r(input, " ", &inputptr);
-            //args->nextarg = NULL;
-            struct stat stt;
-            if(currarg != NULL && strstr(currarg, "/") != NULL){
-                //debug("trying relative/absolute path %d", 0);
-                int success = stat(currarg, &stt);
-                if(!success){
-                    prepexecute(currarg, inputptr);
-                }
-            }
-            else if(currarg != NULL){
-                char path[] = {0};
-                strcat(path, getenv("PATH"));
-                char *pathptr;
-                char *pathtok = strtok_r(path, ":", &pathptr);
-                char execpath[256] = {0};
-                strcat(strcat(strcat(execpath, pathtok), "/"), currarg);
-                //debug("current exec path: %s\n", execpath);
-                int success = stat(execpath, &stt);
-                //debug("success: %d %d\n", success, errno);
-                while(success && pathtok != NULL){
-                    pathtok = strtok_r(NULL, ":", &pathptr);
-                    if(pathtok == NULL)
-                        break;
-                    memset(execpath, 0, 256);
-                    strcat(strcat(strcat(execpath, pathtok), "/"), currarg);
-                    //debug("current exec path: %s\n", execpath);
-                    success = stat(execpath, &stt);
-                    //debug("success: %d %d\n", success, errno);
-                }
-                if(!success){
-                    //debug("exec path: %s\n", execpath);
-                    prepexecute(execpath, inputptr);
-                }
-                else{
-                    printf(EXEC_NOT_FOUND, input);
-                }
-
-            }
-            else{
-                debug("printing else case %d", 0);
-                printf(EXEC_NOT_FOUND, input);
-            }
+            char *token;
+            //ExecStruct **exec = calloc(1, sizeof(ExecStruct))
+            token = strtok_r(input, "|", &tokptr);
+            token = trimwhitespace(token);
+            findexecutable(token);
         }
 
         // Readline mallocs the space for input. You must free it.
@@ -216,7 +201,84 @@ int main(int argc, char *argv[], char* envp[]) {
     return EXIT_SUCCESS;
 }
 
-int prepexecute(char *currarg, char *inputptr){
+char *trimwhitespace(char *token){
+    while(*token == ' ' || *token == '\t'){
+        *token = 0;
+        token++;
+    }
+    //debug("%s", token);
+    int end = strlen(token)-1;
+    while(*(token+end) == ' ' || *(token+end) == '\t'){
+        *(token+end) = 0;
+        end--;
+    }
+    //debug("%s", token);
+    return token;
+}
+
+int findexecutable(char *input){
+    //ArgStruct *args;
+    outpos = strstr(input, ">");
+    inpos = strstr(input, "<");
+    if(outpos > 0){
+        *outpos = 0;
+        outpos++;
+    }
+    if(inpos > 0){
+        *inpos = 0;
+        inpos++;
+    }
+    char *inputptr;
+    char *currarg = strtok_r(input, " ", &inputptr);
+    //args->nextarg = NULL;
+    struct stat stt;
+    if(currarg != NULL && strstr(currarg, "/") != NULL){
+        debug("trying relative/absolute path %d", 0);
+        int success = stat(currarg, &stt);
+        if(!success){
+            prepexecute(currarg, currarg, inputptr);
+        }
+    }
+    else if(currarg != NULL){
+        char *path = malloc(strlen(getenv("PATH")));
+        strcpy(path, getenv("PATH"));
+        //debug("%s", path);
+        char *pathptr;
+        char *pathtok = strtok_r(path, ":", &pathptr);
+        //debug("%s", pathtok);
+        char execpath[256] = {0};
+        //debug("%s", pathtok);
+        strcat(strcat(strcat(execpath, pathtok), "/"), currarg);
+        //debug("current exec path: %s\n", execpath);
+        int success = stat(execpath, &stt);
+        //debug("success: %d %d\n", success, errno);
+        while(success < 0 && pathtok != NULL){
+            pathtok = strtok_r(NULL, ":", &pathptr);
+            if(pathtok == NULL)
+                break;
+            memset(execpath, 0, 256);
+            strcat(strcat(strcat(execpath, pathtok), "/"), currarg);
+            //debug("current exec path: %s\n", execpath);
+            success = stat(execpath, &stt);
+            //debug("success: %d %d\n", success, errno);
+        }
+        if(!success){
+            //debug("exec path: %s\n", execpath);
+            prepexecute(execpath, currarg, inputptr);
+        }
+        else{
+            printf(EXEC_NOT_FOUND, input);
+        }
+        free(path);
+    }
+    else{
+        debug("printing else case %d", 0);
+        printf(EXEC_NOT_FOUND, input);
+    }
+    return EXIT_SUCCESS;
+}
+
+int prepexecute(char *execpath, char *currarg, char *inputptr){
     if(strstr(currarg, "./") == currarg){
         currarg += 2;
     }
@@ -237,28 +299,140 @@ int prepexecute(char *currarg, char *inputptr){
     }
     argv = realloc(argv, sizeof(char *) * (counter + 1));
     argv[counter] = NULL;
-    execute(argv[0], argv);
-    debug("did the thing %d\n", 0);
+    int error = execute(execpath, argv);
+    memset(pipefd1, -1, sizeof(int) * 2);
+    memset(pipefd2, -1, sizeof(int) * 2);
+    error = error;
+    debug("did the thing %d\n", error);
+    free(argv);
     return EXIT_SUCCESS;
+}
+
+int checkredirection(char *currarg)
+{
+    if(strstr(currarg, "<")==0){
+        return INPUT_REDIRECTION;
+    }
+    else if(strcmp(currarg, ">")==0){
+        return OUTPUT_REDIRECTION;
+    }
+    else if(strcmp(currarg, "<")==0){
+        return PIPING_REDIRECTION;
+    }
+    else{
+        return NO_REDIRECTION;
+    }
 }
 
 int execute(const char *pathname, char *vargs[]){
     sigprocmask(SIG_BLOCK, &mask, &prev);
-    if(fork() == 0){
+    pid = 0;
+    char *token = strtok_r(NULL, "|", &tokptr);
+    debug("%d, %d", pipefd1[0], pipefd1[1]);
+    if(token != NULL){
+        pipe(pipefd2);
+        debug("%d, %d", pipefd2[0], pipefd2[1]);
+    }
+    if((childpid = fork()) == 0){
         sigprocmask(SIG_SETMASK, &prev, NULL);
+        //debug("%d", redirection);
+        int inputfd = pipefd1[0];
+        int outputfd = pipefd2[1];
+        //debug("%d", redirection);
+        debug("doing this? %ld", (long)outpos);
+        if(inpos > 0 && (inpos < outpos || !outpos) && inputfd < 0){
+            debug("%s",inpos);
+            inpos = trimwhitespace(inpos);
+            debug("%s",inpos);
+            debug("%zd", strlen(inpos));
+            inputfd = open(inpos, O_RDONLY);
+            if(inputfd < 0){
+                debug("%d", inputfd);
+                printf(SYNTAX_ERROR, "invalid file name");
+                exitfork(EXIT_FAILURE, inputfd, outputfd);
+            }
+        }
+        if(outpos > 0 && outputfd < 0){
+            debug("%s", outpos);
+            outpos = trimwhitespace(outpos);
+            debug("%s", outpos);
+            outputfd = open(outpos, O_RDWR | O_CREAT | O_TRUNC, 0666);
+            if(outputfd < 0){
+                debug("%d", errno);
+                printf(SYNTAX_ERROR, "invalid file name");
+                exitfork(EXIT_FAILURE, inputfd, outputfd);
+            }
+        }
+        //debug("%d", redirection);
+        if(inputfd >= 0){
+            debug("duping %d", inputfd);
+            dup2(inputfd, STDIN_FILENO);
+        }
+        if(outputfd >= 0){
+            debug("duping %d", outputfd);
+            dup2(outputfd, STDOUT_FILENO);
+        }
+        //close(pipefd1[0]);
+        //close(pipefd1[1]);
+        //close(pipefd2[0]);
+        //close(pipefd2[1]);
         char execpath[256] = {0};
         if(strstr(pathname, "/") == pathname)
             strcat(execpath, pathname);
         else
             strcat(strcat(strcat(execpath, currentDir), "/"), pathname);
         int error = execv(pathname, vargs);
-        if(error == -1)
+        debug("%s", "program executed");
+        if(error == -1){
             debug("%d, %d\n", error, errno);
-        exit(EXIT_SUCCESS);
+            exitfork(EXIT_FAILURE, inputfd, outputfd);
+        }
+
+        if(inpos > 0 && inpos > outpos && inputfd < 0){
+            inpos = trimwhitespace(inpos);
+            inputfd = open(inpos, O_RDONLY);
+            dup2(inputfd, STDIN_FILENO);
+            if(inputfd < 0){
+                //debug("%d", redirection);
+                printf(SYNTAX_ERROR, "invalid file name");
+                exit(EXIT_FAILURE);
+            }
+            else{
+                char c;
+                while((c = getchar()) != EOF)
+                    putchar(c);
+            }
+        }
+        exitfork(EXIT_SUCCESS, inputfd, outputfd);
     }
-    sigsuspend(&prev);
+    else{
+        signal(SIGINT, sigint_handler);
+    }
+    //close(pipefd1[0]);
+    //close(pipefd1[1]);
+    //close(pipefd2[0]);
+    close(pipefd2[1]);
+    while(pid != childpid)
+        sigsuspend(&prev);
     //for now just reap the child
     sigprocmask(SIG_SETMASK, &prev, NULL);
+    signal(SIGINT, SIG_DFL);
+    memcpy(pipefd1, pipefd2, sizeof(int) * 2);
+    memset(pipefd2, -1, sizeof(int) * 2);
+    if(token != NULL){
+        token = trimwhitespace(token);
+        findexecutable(token);
+    }
     //wait(NULL);
     return EXIT_SUCCESS;
+}
+
+void exitfork(int success, int inputfd, int outputfd){
+    if(inputfd>=0)
+        close(inputfd);
+    if(outputfd>=0){
+        debug("%s", "output closed");
+        close(outputfd);
+    }
+    exit(success);
 }
